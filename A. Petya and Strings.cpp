a94bf9b1c6fa3da79cb63ab85fedfee7f8270f3e
AbdallahHemdan <40190772+AbdallahHemdan@users.noreply.github.com>
2018-10-14 16:23:20 +0200

    Create A. Petya and Strings.cpp

---
+++ A. Petya and Strings.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <string.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <iomanip>
+#include <assert.h>
+#include <vector>
+#include <cstring>
+#include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <set>
+#include <complex>
+#include <list>
+#include <climits>
+#include <cctype>
+#include <bitset>
+#include <numeric>
+#include<array>
+#include<tuple>
+#include <utility>
+#include <functional>
+#include <locale>
+#define     all(v)            v.begin(),v.end()
+#define     mp                     make_pair
+#define     pb                     push_back
+typedef   long long int               ll ;
+using namespace std;
+int main()
+{
+    string a , b ;
+    cin >> a >> b ;
+    transform(a.begin(),a.end(),a.begin(),::tolower ) ;
+    transform(b.begin(),b.end(),b.begin(),::tolower ) ;
+    if(a<b)
+        cout << -1 << endl ;
+    else if (b<a)
+        cout << 1 << endl ;
+    else
+        cout << 0 << endl ;
+
+}
