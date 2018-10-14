8a414badb3d50cc6d858cb17fe11404d70dd6f96
AbdallahHemdan <40190772+AbdallahHemdan@users.noreply.github.com>
2018-10-14 16:06:26 +0200

    Create A. Domino piling.cpp

---
+++ A. Domino piling.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <string.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <iomanip>
+#include <assert.h>
+#include <vector>
+#include <cstring>
+#include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <set>
+#include <complex>
+#include <list>
+#include <climits>
+#include <cctype>
+#include <bitset>
+#include <numeric>
+#include<array>
+#include<tuple>
+#include <utility>
+#include <functional> 
+#include <locale>  
+using namespace std ;
+int main()
+{
+    int n , m ; 
+    cin >> n >> m ; 
+    cout << floor(n*m*0.5) << endl ;
+}
