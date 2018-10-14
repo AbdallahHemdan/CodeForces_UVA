889403fa9134069a10980fb2eb13e0c6817c85b0
AbdallahHemdan <40190772+AbdallahHemdan@users.noreply.github.com>
2018-10-14 16:17:26 +0200

    Update A. The Rank.cpp

--- A. The Rank.cpp
+++ A. The Rank.cpp
@@ -31,25 +31,16 @@
 using namespace std ;
 int main()
 {
-    long long int size; 
-    cin >>size ; 
-    string s1 , s2 ; 
-    cin >> s1 >> s2 ; 
-    long long int num_of_ones=0; 
-    long long int num_of_zeros=0 ;
-    long long int ans =0 ; 
-    for(long long int i=0 ; i<size ;i++)
-        (s1[i]=='1')?num_of_ones++ : num_of_zeros++ ; 
-    for(long long int i=0 ; i<size ; i++){
-    // case 1 
-    if(s1[i]=='0' && s2[i]=='0'){
-        ans+=num_of_ones ; 
-        num_of_zeros-- ; 
-    }
-    else if (s1[i]='1' && s2[i]=='0'){ 
-        ans+=num_of_zeros ;   
-        num_of_ones-- ; 
-        }     
-    }
-    cout << ans <<endl ; 
-}
+    int size ; 
+    cin >> size ; 
+    int a , b , c ,d ; 
+    cin >> a >> b >> c >> d ; 
+     int s = a+b+c+d ;
+     int rank = 1 ; 
+     for(int i=1 ; i<size ;i++)
+          {
+              cin >> a >> b >> c >> d ;
+              if(a+b+c+d>s)rank++ ; 
+          }
+          cout << rank << endl ;
+}          
