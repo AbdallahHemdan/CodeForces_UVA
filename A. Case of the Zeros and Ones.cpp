adbab33af0810059d47a9d063a2943b31678d912
AbdallahHemdan <40190772+AbdallahHemdan@users.noreply.github.com>
2018-10-14 16:24:47 +0200

    Create A. Case of the Zeros and Ones.cpp

---
+++ A. Case of the Zeros and Ones.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <string.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <iomanip>
+#include <assert.h>
+#include <vector>
+#include <cstring>
+#include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <set>
+#include <complex>
+#include <list>
+#include <climits>
+#include <cctype>
+#include <bitset>
+#include <numeric>
+#include<array>
+#include<tuple>
+#include <utility>
+#include <functional>
+#include <locale>
+#define     all(v)            v.begin(),v.end()
+#define     mp                     make_pair
+#define     pb                     push_back
+typedef   long long int               ll ;
+using namespace std ;
+int main()
+{
+    int len ;
+    cin >> len ;
+    string s ;
+    cin >> s ;
+    int ones=0 , zeros=0 ;
+    for(int i=0 ;i<s.size();i++)
+        {
+            (s[i]=='1')?ones++ :zeros++ ;
+        }
+        cout << len-2*min(ones,zeros) << endl;
+}
