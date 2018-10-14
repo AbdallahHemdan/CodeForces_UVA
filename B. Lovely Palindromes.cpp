78521be80f77590601e09396c67005db22f315d2
AbdallahHemdan <40190772+AbdallahHemdan@users.noreply.github.com>
2018-10-14 16:22:38 +0200

    Create B. Lovely Palindromes.cpp

---
+++ B. Lovely Palindromes.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <string.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <iomanip>
+#include <assert.h>
+#include <vector>
+#include <cstring>
+#include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <set>
+#include <complex>
+#include <list>
+#include <climits>
+#include <cctype>
+#include <bitset>
+#include <numeric>
+#include<array>
+#include<tuple>
+#include <utility>
+#include <functional>
+#include <locale>
+#define     all(v)            v.begin(),v.end()
+#define     mp                     make_pair
+#define     pb                     push_back
+typedef   long long int               ll ;
+using namespace std ;
+ int main()
+ {
+     string str ;
+     cin >> str ;
+     string temp = str ;
+     reverse(temp.begin(),temp.end());
+     cout << str+temp << endl ;
+
+ }
