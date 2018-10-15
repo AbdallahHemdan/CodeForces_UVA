0444ed44830453328e45da3376d5020dba9914cc
AbdallahHemdan <40190772+AbdallahHemdan@users.noreply.github.com>
2018-10-15 23:03:31 +0200

    Create W. Pythagorean Theorem II.cpp

---
+++ W. Pythagorean Theorem II.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <string.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <iomanip>
+#include <assert.h>
+#include <vector>
+#include <cstring>
+#include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <set>
+#include <complex>
+#include <list>
+#include <climits>
+#include <cctype>
+#include <bitset>
+#include <numeric>
+#include<array>
+#include<tuple>
+#include <utility>
+#include <functional>
+#include <locale>
+#define     all(v)            v.begin(),v.end()
+#define     mp                     make_pair
+#define     pb                     push_back
+typedef   long long int               ll ;
+
+using namespace std ; 
+
+int main(){
+
+int n  ; 
+cin >> n ; 
+int counter = 0 , temp ; 
+for(int i=1 ; i<=n ; i++){
+    for(int j=i ; (i*i)+(j*j)<=(n*n) ; j++){
+        temp = i*i + j*j ;     
+        if(int(sqrt(temp))*int(sqrt(temp)) == temp)
+            counter++ ; 
+        }
+    }
+cout << counter << endl ;
+}
