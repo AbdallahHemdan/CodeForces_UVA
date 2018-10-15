3a7330ebc72d4d1c2e9185e0a108c0eef5016914
AbdallahHemdan <40190772+AbdallahHemdan@users.noreply.github.com>
2018-10-15 22:40:43 +0200

    Create B. Registration system.cpp

---
+++ B. Registration system.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <string.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <iomanip>
+#include <assert.h>
+#include <vector>
+#include <cstring>
+#include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <set>
+#include <complex>
+#include <list>
+#include <climits>
+#include <cctype>
+#include <bitset>
+#include <numeric>
+#include<array>
+#include<tuple>
+#include <utility>
+#include <functional>
+#include <locale>
+#define     all(v)            v.begin(),v.end()
+#define     mp                     make_pair
+#define     pb                     push_back
+typedef   long long int               ll ;
+
+using namespace std ; 
+int main(){
+
+int n ; 
+cin >> n ; 
+map<string,int> mp ; 
+string str ; 
+for(int i=0 ;i<n ;i++){
+    cin >> str ; 
+    if(mp[str] > 0)
+        cout << str << mp[str] << endl ;
+    else 
+        cout << "OK" << endl ;
+    mp[str]++ ;     
+    
+}
+   
+}
