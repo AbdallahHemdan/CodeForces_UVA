8f25442c62b965901f600c8839a90abecb77f06a
AbdallahHemdan <40190772+AbdallahHemdan@users.noreply.github.com>
2018-10-15 23:04:12 +0200

    Create X. Tricky Sum.cpp

---
+++ X. Tricky Sum.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <string.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <iomanip>
+#include <assert.h>
+#include <vector>
+#include <cstring>
+#include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <set>
+#include <complex>
+#include <list>
+#include <climits>
+#include <cctype>
+#include <bitset>
+#include <numeric>
+#include<array>
+#include<tuple>
+#include <utility>
+#include <functional>
+#include <locale>
+#define     all(v)            v.begin(),v.end()
+#define     mp                     make_pair
+#define     pb                     push_back
+typedef   long long int               ll ;
+
+using namespace std ;
+int main(){
+    
+int t; 
+cin >> t ; 
+
+while(t--){
+    ll n ; 
+    cin >> n ;
+ll sum = (n*(n+1)/2)-1*2 ; 
+for(int i=2 ; i<=n ; i*=2)
+    sum-=i*2 ; 
+cout << sum<< endl ;
+    
+    }
+}
