09babf8a3938f89c10fa6f6fa531cfc5fea45d00
Abdallah Hemdan <40190772+AbdallahHemdan@users.noreply.github.com>
2019-04-02 05:55:08 +0200

    Create A - Stones on the Table.cpp

---
+++ Junior-Sheet/CF-A/A - Stones on the Table.cpp
@@ -0,0 +1,52 @@
+/*Author : Abdallah Hemdan */ 
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <string.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <iomanip>
+#include <assert.h>
+#include <vector>
+#include <cstring>
+#include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <sstream>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <set>
+#include <complex>
+#include <list>
+#include <climits>
+#include <cctype>
+#include <bitset>
+#include <numeric>
+#include<array>
+#include<tuple>
+#include <utility>
+#include <functional>
+#include <locale>
+#define     all(v)            v.begin(),v.end()
+#define     mp                     make_pair
+#define     pb                     push_back
+typedef   long long int               ll ;
+
+//freopen("input.txt","r",stdin);
+//freopen("output.txt","w",stdout);
+
+
+using namespace std ;
+int main(){
+    int n ; 
+    cin >> n ; 
+    string s ; 
+    cin >> s ; 
+    int counter = 0 ; 
+    for(int i=0 ;i<s.size()-1;i++)
+        if(s[i]==s[i+1])
+            counter++ ; 
+    cout << counter << endl ;
+}
